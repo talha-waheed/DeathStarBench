5c3fc8b44a80b9ed6f87b36d381cfb4a32d2a2b8
Aditya Prerepa <adiprerepa@gmail.com>
2024-04-10 16:19:25 -0500

    set oprot?

--- socialNetwork/docker/openresty-thrift/lua-thrift/RpcClientFactory.lua
+++ socialNetwork/docker/openresty-thrift/lua-thrift/RpcClientFactory.lua
@@ -11,7 +11,7 @@ function RpcClientFactory:createClient(thriftClient, ip, port, timeout, ssl)
     local protocol = self:init(ip, port, timeout, ssl)
     local client = thriftClient:new{
         iprot = protocol,
-        -- oprot = protocol
+        oprot = protocol
     }
     return client
 end
